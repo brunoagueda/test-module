8425cc254fa5d54db4e9797dceb95362653de38d
Bruno Agueda <brunoagueda@gmail.com>
2022-08-02 22:02:08 -0300

    Usando o terraform para editar as env vars do Lambda

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ resource "aws_lambda_function" "test_lambda" {
   
   function_name = "PSTest"
   role          = "LambdaRoleForCodePipeline"
-  
+  handler       = "PSTest::PSTest.Bootstrap::ExecuteFunction"
+  runtime       = "dotnet6"
   environment {
     variables = {
       runnerDevice = var.runner_Device
